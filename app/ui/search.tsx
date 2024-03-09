1840e1ec15e0e3a3f2f34c5c79e64f531326e974
Zadorin Artyom <noardour@gmail.com>
2024-03-09 14:40:54 +0500

    search and pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,28 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { ChangeEventHandler } from 'react';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch: ChangeEventHandler<HTMLInputElement> =
+    useDebouncedCallback((e) => {
+      console.log(e.target.value);
+      const params = new URLSearchParams(searchParams);
+      params.set('page', '1');
+      if (e.target.value) {
+        params.set('query', e.target.value);
+      } else {
+        params.delete('query');
+      }
+      replace(`${pathname}?${params.toString()}`);
+    }, 400);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +31,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={handleSearch}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
